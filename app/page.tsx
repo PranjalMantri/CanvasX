eefaf2bbbf62e6e3649fdf15315e425073c47d21
Pranjal Mantri <pranjalmantri@gmail.com>
2025-10-25 21:56:05 +0530

    feat: basic canvas display

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,44 @@
+"use client";
+
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const [tool, setTool] = useState("line");
+
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    setDimensions({ width: window.innerWidth, height: window.innerHeight });
+  }, []);
+
+  useLayoutEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const context = canvas.getContext("2d");
+    if (!context) return;
+
+    requestAnimationFrame(() => {
+      context.strokeStyle = "black";
+      context.lineWidth = 2;
+
+      context.strokeRect(10, 10, 150, 150);
+
+      context.beginPath();
+      context.moveTo(10, 10);
+      context.lineTo(110, 110);
+      context.stroke();
+    });
+  }, [dimensions]);
+
+  return (
+    <div>
+      <canvas
+        ref={canvasRef}
+        width={dimensions.width}
+        height={dimensions.height}
+      ></canvas>
+    </div>
+  );
 }
