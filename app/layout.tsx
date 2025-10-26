a9745d1b0cd123bdd6dd07012292697c3ef5a1f6
Pranjal Mantri <pranjalmantri@gmail.com>
2025-10-26 19:07:54 +0530

    feat: add favicon and update metadata for icon display

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "CanvasX",
   description: "Realtime Collaborative Whiteboard App",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
